466969fa94bbfdbb1470799d11608875334f5d58
Keane Pereira <keane3pereira@gmail.com>
2021-04-10 17:09:09 +0530

    port

--- index.js
+++ index.js
@@ -48,6 +48,6 @@ app.post('/ask', function(req,res){
     console.log(req.body);    
 });
 
-app.listen(3000, function(){
+app.listen(process.env.port || 3000, function(){
     console.log('Server started at port 3000');
 });
